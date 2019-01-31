c48a39eb6b64a853ee2a0520c5ef947bb43794ea
CM Student <cmstudent@uhwo-acm-10.local>
2019-01-31 10:54:20 -1000

    If/Else Statement Completed

--- IfElseStatements/ViewController.swift
+++ IfElseStatements/ViewController.swift
@@ -8,13 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class viewController: UIViewController {
+var firstString = "The background color will turn to blue"
+    var secondString = "The background color will turn to green"
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+    @IBAction func changeColorButtonPressed(_ sender: Any) {
+    
+if firstString == ("The background color will turn red"){
+    self.view.backgroundColor = UIColor.red
+    
+    
+    }else{
+self.view.backgroundColor = UIColor.blue
 }
 
+}
+
+}
